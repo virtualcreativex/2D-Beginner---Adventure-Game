8007e020b4ca395ff1720e043dfdf1601141fb60
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-08 17:43:42 -0500

    1.8. - Test your changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,5 +12,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Vector2 position = transform.position;
+        position.x = position.x + 0.1f;
+        transform.position = position;
     }
 }
