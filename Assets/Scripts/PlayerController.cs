5b78a4077c7532aedbe4c53b61e9790a52d0bcca
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-08 19:55:57 -0500

    Tutorial 3.4 - Create Input Actions for player character movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
+    //  Update is called once per frame
     void Update()
     {
         float horizontal = 0.0f;
@@ -25,10 +25,21 @@ public class PlayerController : MonoBehaviour
         Debug.Log(horizontal);
 
 
+        float vertical = 0.0f;
+        if (Keyboard.current.upArrowKey.isPressed)
+        {
+            vertical = 1.0f;
+        }
+        else if (Keyboard.current.downArrowKey.isPressed)
+        {
+            vertical = -1.0f;
+        }
+        Debug.Log(vertical);
 
 
         Vector2 position = transform.position;
         position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + 0.1f * vertical;
         transform.position = position;
     }
 }
