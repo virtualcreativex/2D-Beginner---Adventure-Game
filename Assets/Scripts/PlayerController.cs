d0332bdd7cbe89c62d922cb3244652010da4f5ce
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-08 19:52:50 -0500

    3.3.4 - Apply horizontal movement to the player character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,8 +13,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        Debug.Log(horizontal);
+
+
+
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
         transform.position = position;
     }
 }
