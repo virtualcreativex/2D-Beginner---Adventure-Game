37189009de9d9403164660a48bfb9ae59dfffd76
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-08 20:39:13 -0500

    3.5.6. - Check your Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
     public InputAction MoveAction;
-
 
     // Start is called before the first frame update
     void Start()
