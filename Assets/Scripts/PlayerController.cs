26d7e0a9601f8f1791a8f66f878ab8ea3d9bfbe3
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-10 16:51:17 -0500

    Tutorial 5.1 - Set up a basic health system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,20 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    
+    public int maxHealth = 5;
+    int currentHealth;
+    
+    
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();  
         rigidbody2d = GetComponent<Rigidbody2D>();
+        
+        currentHealth = maxHealth;
+        
     }
     
     // Update is called once per frame
