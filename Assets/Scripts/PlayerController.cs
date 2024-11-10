a8f46c531b636519f1b713851b6759917025755c
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-10 17:05:08 -0500

    5.1.6. - Check your script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,24 +4,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    // Variables related to player character movement
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
-    
+    public float speed = 3.0f;
+
+    // Variables related to the health system
     public int maxHealth = 5;
     int currentHealth;
-    
-    
 
     // Start is called before the first frame update
     void Start()
     {
-        MoveAction.Enable();  
+        MoveAction.Enable();
         rigidbody2d = GetComponent<Rigidbody2D>();
-        
         currentHealth = maxHealth;
     }
-    
+ 
     // Update is called once per frame
     void Update()
     {
@@ -29,17 +29,16 @@ public class PlayerController : MonoBehaviour
         Debug.Log(move);
     }
 
+    // FixedUpdate has the same call rate as the physics system 
     void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * (3.0f * Time.deltaTime);
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
-    
+
     void ChangeHealth (int amount)
     {
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
-    
-    
 }
