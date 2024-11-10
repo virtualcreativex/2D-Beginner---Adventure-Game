c9f061f3d23f086acea3ff56150137b2f3c4b3b7
Monica Yaneth Loeb Willes <manager@virtualcreativex.com>
2024-11-09 20:40:35 -0500

    4.3.8. - Fix the PlayerCharacter jittering

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,33 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public InputAction MoveAction;
+    
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();  
+
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
     }
     
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * (3.0f * Time.deltaTime);
-        transform.position = position;
     }
-    
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
 }
